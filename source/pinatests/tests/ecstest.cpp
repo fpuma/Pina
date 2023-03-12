ed783c6aa29c553d2ed65742a24254e9e3b2fe2c
fpuma <fpuma88@gmail.com>
2023-03-12 21:31:24 +0100

    Adding component usage tests

--- source/pinatests/tests/ecstest.cpp
+++ source/pinatests/tests/ecstest.cpp
@@ -134,105 +134,86 @@ TEST( ECS, ComponentLifetime )
     EXPECT_NE( compA2, nullptr );
 
     compProvider->remove<ComponentA>( ntt1 );
-    compProvider->remove<ComponentA>( ntt2 );
-
+    
     EXPECT_FALSE( compProvider->contains<ComponentA>( ntt1 ) );
+
+    nttProvider->disposeEntity( ntt2 );
+
     EXPECT_FALSE( compProvider->contains<ComponentA>( ntt2 ) );
+    EXPECT_FALSE( compProvider->contains<ComponentB>( ntt2 ) );
+    EXPECT_FALSE( compProvider->contains<ComponentC>( ntt2 ) );
+
 
 #ifdef _DEBUG
+    EXPECT_DEATH( compProvider->get<ComponentA>( ntt2 ), "m_elements.end" );
     EXPECT_DEATH( nttProvider->uninit(), "use_count" );
     safeCompA0.reset();
     nttProvider->uninit();
 #else
+    compA2 = compProvider->get<ComponentA>( ntt2 );
+    EXPECT_EQ( compA2, nullptr );
     nttProvider->uninit();
 #endif     
 }
 
-//TEST( ECS, Tests )
-//{
-//    puma::pina::ECS ecs;
-//
-//    puma::pina::EntityProvider* nttProvider = ecs.getEntityProvider();
-//    puma::pina::ComponentProvider* componentProvider = ecs.getComponentProvider();
-//    
-//    componentProvider->registerClass<ComponentA>();
-//    componentProvider->registerClass<ComponentB>();
-//
-//    puma::pina::Entity entity0 = nttProvider->requestEntity();
-//
-//#ifdef _DEBUG
-//    EXPECT_DEATH( componentProvider->get<ComponentA>( entity0 ), "itElement != m_elements.end" );
-//#else
-//    EXPECT_EQ( nullptr, componentProvider->get<ComponentA>( entity0 ) );
-//#endif
-//
-//    EXPECT_FALSE( componentProvider->contains<ComponentA>( entity0 ) );
-//
-//    std::shared_ptr<ComponentA> ca0 = componentProvider->add<ComponentA>( entity0 );
-//
-//    EXPECT_NE( nullptr, ca0 );
-//
-//    puma::pina::Entity entity1 = nttProvider->requestEntity();
-//    puma::pina::Entity entity2 = nttProvider->requestEntity();
-//    puma::pina::Entity entity3 = nttProvider->requestEntity();
-//
-//    auto ca1 = componentProvider->add<ComponentA>( entity1 );
-//
-//    auto ca2 = componentProvider->add<ComponentA>( entity2 );
-//    auto cb2 = componentProvider->add<ComponentB>( entity2 );
-//
-//    auto cb3 = componentProvider->add<ComponentB>( entity3 );
-//
-//    //EXPECT_EQ( 1, ca0->getNumber() );
-//
-//    //EXPECT_EQ( 2, ca0->getNumber() );
-//    //EXPECT_EQ( 1, ca1->getNumber() );
-//    //EXPECT_EQ( 1, ca2->getNumber() );
-//
-//    //EXPECT_EQ( 2, ca0->getNumber() );
-//    //EXPECT_EQ( 1, ca1->getNumber() );
-//    //EXPECT_EQ( 1, ca2->getNumber() );
-//    //EXPECT_EQ( 1, cb2->getNumber() );
-//    //EXPECT_EQ( 1, cb3->getNumber() );
-//
-//    nttProvider->disableEntity( entity2 );
-//    //EXPECT_FALSE( ca2->isEnabled() );
-//    //EXPECT_FALSE( cb2->isEnabled() );
-//
-//    nttProvider->enableEntity( entity2 );
-//
-//    EXPECT_TRUE( ca2->isEnabled() );
-//    EXPECT_TRUE( cb2->isEnabled() );
-//
-//    //EXPECT_EQ( 2, ca2->getNumber() );
-//    //EXPECT_EQ( 2, cb2->getNumber() );
-//
-//
-//    //ENTITIES BY COMPONENTS
-//    auto ntts = ecs.getEntitesByComponents<ComponentA, ComponentB>();
-//    //EXPECT_EQ( ntts.size(), 1 );
-//    //EXPECT_EQ( *ntts.begin(), entity2);
-//
-//    componentProvider->add<ComponentA>( entity3 );
-//    ntts = ecs.getEntitesByComponents<ComponentA, ComponentB>();
-//
-//    //EXPECT_EQ( ntts.size(), 2 );
-//    auto nttsBegin = ntts.begin();
-//    //EXPECT_EQ( *nttsBegin, entity2 );
-//    //std::advance( nttsBegin, 1 );
-//    //EXPECT_EQ( *nttsBegin, entity3 );
-//
-//
-//    //UNINIT
-//    componentProvider->remove<ComponentA>( entity0 );
-//    componentProvider->remove<ComponentA>( entity1 );
-//    componentProvider->remove<ComponentA>( entity2 );
-//    componentProvider->remove<ComponentB>( entity2 );
-//    componentProvider->remove<ComponentA>( entity3 );
-//    componentProvider->remove<ComponentB>( entity3 );
-//
-//    nttProvider->disposeEntity( entity0 );
-//    nttProvider->disposeEntity( entity1 );
-//    nttProvider->disposeEntity( entity2 );
-//    nttProvider->disposeEntity( entity3 );
-//}
+TEST( ECS, ComponentsUsage )
+{
+    puma::pina::ECS ecs;
+    puma::pina::EntityProvider* nttProvider = ecs.getEntityProvider();
+    puma::pina::ComponentProvider* compProvider = ecs.getComponentProvider();
+
+    nttProvider->init( 20 );
+
+    puma::pina::Entity ntt0 = nttProvider->requestEntity();
+    puma::pina::Entity ntt1 = nttProvider->requestEntity();
+    puma::pina::Entity ntt2 = nttProvider->requestEntity();
+
+    compProvider->registerClass<ComponentA>();
+    compProvider->registerClass<ComponentB>();
+    compProvider->registerClass<ComponentC>();
+
+    compProvider->add<ComponentA>( ntt0 );
+    compProvider->add<ComponentB>( ntt0 );
+
+    compProvider->add<ComponentA>( ntt1 );
+    compProvider->add<ComponentC>( ntt1 );
+
+    compProvider->add<ComponentA>( ntt2 );
+    compProvider->add<ComponentB>( ntt2 );
+    compProvider->add<ComponentC>( ntt2 );
+
+    auto aNtts = ecs.getEntitesByComponents<ComponentA>();
+    auto abNtts = ecs.getEntitesByComponents<ComponentA, ComponentB>();
+    auto acNtts = ecs.getEntitesByComponents<ComponentA, ComponentC>();
+    auto abcNtts = ecs.getEntitesByComponents<ComponentA, ComponentB, ComponentC>();
+
+    EXPECT_EQ( aNtts.size(), 3 );
+    EXPECT_EQ( abNtts.size(), 2 );
+    EXPECT_EQ( acNtts.size(), 2 );
+    EXPECT_EQ( abcNtts.size(), 1 );
+
+    for (const puma::pina::Entity& ntt : abNtts)
+    {
+        EXPECT_TRUE( ntt.value() == 0 || ntt.value() == 2 );
+    }
+
+    for (const puma::pina::Entity& ntt : acNtts)
+    {
+        EXPECT_TRUE( ntt.value() == 1 || ntt.value() == 2 );
+    }
+
+    EXPECT_TRUE( abcNtts.begin()->value() == 2 );
+
+    compProvider->remove<ComponentA>( ntt2 );
+
+    abcNtts = ecs.getEntitesByComponents<ComponentA, ComponentB, ComponentC>();
+    auto bcNtts = ecs.getEntitesByComponents<ComponentB, ComponentC>();
+
+    EXPECT_TRUE( abcNtts.empty() );
+    EXPECT_EQ( bcNtts.size(), 1 );
+
+    EXPECT_TRUE( bcNtts.begin()->value() == 2 );
+
+    nttProvider->uninit();
+
+}
